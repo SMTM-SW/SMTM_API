9f8be38f24aae32265eceac306b95d3f56e3f2ee
Daniel T. Lee <danieltimlee@gmail.com>
2016-11-24 06:38:26 +0900

    Email Sending Success

--- app/api/v1/analyze.py
+++ app/api/v1/analyze.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restful import Resource
+from yagmail import yagmail
 
 from app import api_root, db, app, oauth_provider
 from app.api.api_request import RequestCrawlerAPI
@@ -58,7 +59,7 @@ class Analyze(Resource):
         body = """<div style='background-color:#EEEEEE ;width:540px;background-image:-moz-linear-gradient(top left,#53b2de 0%,#EEEEEE 50%);\
                 font-family:'Lucida Grande','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:18px;color:#555555 '> \
                 <div style='width:500px;padding:20px 20px;margin: 0em auto'> <div style='margin: 20px 175px'> \
-                          <img class="ui banner image" src="http://lookalike.co.kr/static/images/logo_w.png" style="margin:3em auto;"/> </div> \
+                          <img class="ui banner image" src="http://lookalike.co.kr/static/images/logo.png" style="margin:3em auto;"/> </div> \
                 <div style='background-color:#fff;border-radius:2px;padding:40px'> <div style='width:50%;min-height:20%;max-width:300px'> </div> \
                 <div style='width:50%;min-height:20%;max-width:300px'> </div> <span style='font-size:18px;line-height:18px;color:#000000 ;font-weight:normal'> \
                 Lookalike 앱 분석이 시작되었습니다. </span> <br> <br> 현재 분석이 진행중입니다. 분석이 완료되면 이메일이 다시 발송됩니다. <br> <br> <br> \
@@ -74,7 +75,8 @@ class Analyze(Resource):
         body = body.encode('utf-8').decode('utf-8')
 
         user = UserModel.query.filter_by(id=project.user_id).one()
-        # yagmail.SMTP(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']).send(to=[user.email], subject=subject, contents=body)
+        yagmail.SMTP(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']).send(to=[user.email], subject=subject,
+                                                                                    contents=body)
 
         new_notification = NotificationModel(
             content='{0}의 분석이 시작되었습니다.'.format(project.title),
@@ -126,7 +128,7 @@ class Analyze(Resource):
         body = """<div style='background-color:#EEEEEE ;width:540px;background-image:-moz-linear-gradient(top left,#53b2de 0%,#EEEEEE 50%);\
                 font-family:'Lucida Grande','Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:18px;color:#555555 '> \
                 <div style='width:500px;padding:20px 20px;margin: 0em auto'> <div style='margin: 20px 175px'> \
-                <img class="ui banner image" src="http://lookalike.co.kr/static/images/logo_w.png" style="margin:3em auto;"/>  </div> \
+                <img class="ui banner image" src="http://lookalike.co.kr/static/images/logo.png" style="margin:3em auto;"/>  </div> \
                 <div style='background-color:#fff;border-radius:2px;padding:40px'> <div style='width:50%;min-height:20%;max-width:300px'> </div> \
                 <div style='width:50%;min-height:20%;max-width:300px'> </div> <span style='font-size:18px;line-height:18px;color:#000000 ;font-weight:normal'> \
                 Lookalike 앱 분석이 완료되었습니다. </span> <br> <br> 분석이 완료되었습니다. 홈페이지로 이동해서 분석 결과를 확인하세요. <br> <br> <br> \
@@ -142,7 +144,8 @@ class Analyze(Resource):
         body = body.encode('utf-8').decode('utf-8')
 
         user = UserModel.query.filter_by(id=project.user_id).one()
-        # yagmail.SMTP(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']).send(to=[user.email], subject=subject, contents=body)
+        yagmail.SMTP(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']).send(to=[user.email], subject=subject,
+                                                                                    contents=body)
 
         new_notification = NotificationModel(
             content='{0}의 분석이 완료되었습니다.'.format(project.title),
